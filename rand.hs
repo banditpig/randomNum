08749a789e69009669afaa79cdb846fe1e33c4df
banditpig <mike_k_houghton@yahoo.co.uk>
2017-09-24 15:21:27 +0100

    tidy

--- rand.hs
+++ rand.hs
@@ -1,3 +1,4 @@
+-- See http://gitcommit.co.uk/2017/09/24/the-expressiveness-of-haskell-random-numbers/
 import Data.Time.Clock.POSIX
 
 numTodigs :: Integral a => a -> [a]
@@ -5,12 +6,13 @@ numTodigs 0 = []
 numTodigs x = numTodigs (x `div` 10) ++ [x `mod` 10]
 
 pad :: Integral a => [a] -> [a]
-pad xs 
+pad xs
   | length xs >= 8 = xs
   | otherwise = pad (0 : xs)
 
 mid4 :: [a] -> [a]
-mid4 = substr 4 4 
+mid4 = substr 4 4
+
 substr :: Int -> Int -> [a] -> [a]
 substr s e xs = take e (drop s xs)
 
@@ -18,7 +20,7 @@ digsToNum :: [Int] -> Int
 digsToNum ds = eval (10 ^ l) ds
   where
     l = length ds - 1
-    eval _ []     = 0
+    eval _ [] = 0
     eval n (x:xs) = (n * x) + eval (n `div` 10) xs
 
 sq :: Int -> Int
@@ -36,8 +38,3 @@ fourDigits n = n `mod` 10000
 main = do
   n <- sysInt
   print $ take 150 $ iterate vnRan (fourDigits n)
-
-
-
-
-
